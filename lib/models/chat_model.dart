87800844af333898a3ff118203196f88c97e1f93
Harshit Shukla <harshit.shukla@Triconinfotech.com>
2019-01-11 14:44:33 +0530

    Whatsapp kind of UI

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,42 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+  List<ChatModel> dummyData =[
+    new ChatModel(
+      name: "Harshit Shukla",
+      message: "Cloning whatsapp UI",
+      time:"3:30",
+      avatarUrl: "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg"
+    ),
+    new ChatModel(
+        name: "Aman",
+        message: "Let's play pubG",
+        time:"4:30",
+        avatarUrl: "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg"
+    ),
+    new ChatModel(
+        name: "Pinto",
+        message: "I am upstairs!",
+        time:"6:30",
+        avatarUrl: "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg"
+    ),
+    new ChatModel(
+        name: "Utkarsh",
+        message: "Learning AWS services, not getting anything!",
+        time:"7:30",
+        avatarUrl: "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg"
+    ),
+    new ChatModel(
+        name: "Singhal",
+        message: "Machine interacting with machine, future is here!",
+        time:"2:30",
+        avatarUrl: "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg"
+    ),
+  ];
+
